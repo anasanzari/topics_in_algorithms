16c5659ed2e06ff0d8b7ca9ce577d5598d9659e3
Anas Ansari <anasanzari@gmail.com>
2017-01-18 22:03:24 +0530

    try java :P

---
+++ java/src/jav/VertexCover.java
@@ -0,0 +1,24 @@
+package jav;
+
+import java.util.*;
+
+public class VertexCover {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter n:");
+		int n = sc.nextInt();
+		
+		HashMap<Integer, List<Integer>> graph = new HashMap<Integer, List<Integer>>();
+		System.out.println("Enter edges: ");
+		for(int i = 0;i <n;i++){
+			
+		}
+		
+		
+	}
+
+}
